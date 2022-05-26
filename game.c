9280d3dc353a93fc6fb1d74254ba532d73d670af
Huzaifa-Khalid-2 <99287277+Huzaifa-Khalid-2@users.noreply.github.com>
2022-05-26 14:14:51 -0400

    Update game.c

--- game.c
+++ game.c
@@ -4,25 +4,46 @@
 // Created on: March 2022
 // this is the final project
 
-#include <stdio.h>
 #include <gb/gb.h>
+#include <gb/font.h>
+
 #include "background.c"
 #include "backgroundtiles.c"
-#include "Smile.h"
+#include "windowmap.c"
 
-
+void interruptLCD() {
+    HIDE_WIN;
+}
 
 void main() {
-	set_sprite_data(0, 1, TileData);
-	set_sprite_tile(0, 0);
-	move_sprite(0, 88, 78);
-	set_bkg_data(0, 7, backgroundtiles);
-	set_bkg_tiles(0, 0, 40, 18, background);
-	SHOW_BKG;
-	DISPLAY_ON;
+    font_t min_font;
 
-	while(1) {
-		scroll_bkg(1, 0);
-		delay(100);
-	}
+    STAT_REG = 0x45;
+    LYC_REG = 0x08;  //  Fire LCD Interupt on the 8th scan line (just first row)
+
+    disable_interrupts();
+
+    font_init();
+    min_font = font_load(font_min);  // 36 tile
+    font_set(min_font);
+
+    set_bkg_data(37, 7, backgroundtiles);
+    set_bkg_tiles(0, 0, 40, 18, background);
+
+    set_win_tiles(0, 0, 5, 1, windowmap);
+    move_win(7, 0);
+
+    SHOW_BKG;
+    SHOW_WIN;
+    DISPLAY_ON;
+
+    add_LCD(interruptLCD);
+    enable_interrupts();
+    set_interrupts(VBL_IFLAG | LCD_IFLAG);
+
+    while (1) {
+        SHOW_WIN;
+        scroll_bkg(1, 0);
+        wait_vbl_done();
+    }
 }
